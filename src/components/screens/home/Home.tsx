a2a7d2baf29f8218b25242d7b133eaeb1688c48b
VadimMatin <vmartintestng@gmail.com>
2022-08-26 20:42:41 +0300

    addded Accordion component

--- src/components/screens/home/Home.tsx
+++ src/components/screens/home/Home.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Layout from '../../layout/Layout'
 import Cookies from '@src/components/examples/Cookies'
 import Debounce from '@src/components/examples/Debounce'
@@ -11,22 +11,27 @@ import LocalStorage from '@src/components/examples/LocalStorage'
 import Outside from '@src/components/examples/Outside'
 import Escape from '@src/components/examples/Escape'
 import ThemeBgSwitcher from '@src/components/examples/BgColorTheme/ThemeBgSwitcher'
+import Accordion from '@src/components/ui/accordion/Accordion'
 
 const Home: React.FC= () => {
 
+  const [accordionData, _] =  useState([
+    { title: "Cookies", content: <Cookies />},
+    { title: "Debounce", content: <Debounce />},
+    { title: "DidMountEffect", content: <DidMountEffect />},
+    { title: "Toggle", content: <Toggle />},
+    { title: "FetchPost", content: <FetchPost />},
+    { title: "InputFocus", content: <InputFocus />},
+    { title: "WindowSize", content: <WindowSize />},
+    { title: "LocalStorage", content: <LocalStorage />},
+    { title: "Outside", content: <Outside />},
+    { title: "Escape", content: <Escape />},
+    { title: "ThemeBgSwitcher", content: <ThemeBgSwitcher />},
+  ]); 
+
   return (
     <Layout title="Home">
-       <Cookies />
-       <Debounce />
-       <DidMountEffect />
-       <Toggle />
-       <FetchPost />
-       <InputFocus />
-       <WindowSize />
-       <LocalStorage />
-       <Outside />
-       <Escape />
-       <ThemeBgSwitcher />
+       <Accordion items={accordionData} />
     </Layout>
   )
 }
---
+++ src/components/ui/accordion/Accordion.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import './Accordion.less';
+import { AccordionData } from './types';
+import AccordionItem from './AccordionItem';
+
+function Accordion({ items }: { items: Array<AccordionData> }) {
+  
+  const [currentIdx, setCurrentIdx] = useState(-1);
+  const btnOnClick = (idx: number) => {
+    setCurrentIdx(idx);
+  };
+
+  return (
+    <ul className="accordion">
+      {items.map((item, idx) => (
+        <AccordionItem
+          key={idx}
+          data={item}
+          isOpen={idx === currentIdx}
+          btnOnClick={() => btnOnClick(idx)}
+        />
+      ))}
+    </ul>
+  );
+}
+
+export default Accordion;
